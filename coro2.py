891b925687b9b7cfd31b82f55a8a49d78c35b669
Francesco Pierfederici <fpierfed@gmail.com>
2017-06-28 15:06:47 +0200

    sending values improved

--- coro2.py
+++ coro2.py
@@ -1,13 +1,24 @@
 def say(msg='Hello, world'):
+    i = 0
+
     while True:
+        print(f'[coroutine] Iteration {i}')
+
         msg = yield msg
         if msg is None:
             break
 
+        print(f'[coroutine] Iteration {i - 1} end')
+        i += 1
+
 
 if __name__ == '__main__':
     coro = say()
+    print('[system] Calling send method')
     print(coro.send(None))
+    print('[system] Calling send method')
     print(coro.send('Foo!'))
+    print('[system] Calling send method')
     print(coro.send('Bar!'))
+    print('[system] Calling send method')
     print(coro.send(None))
