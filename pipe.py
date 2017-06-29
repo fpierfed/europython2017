1efb4181aeaa40843146496d7ae735a9f629c2b5
Francesco Pierfederici <fpierfed@gmail.com>
2017-06-29 15:50:37 +0200

    Getting more fancy

--- pipe.py
+++ pipe.py
@@ -9,64 +9,78 @@ RESOLUTION = 3
 
 
 def runner(argv, timeout=0):
-    _id = str(uuid.uuid4())
-
-    t0 = time.time()
-    print(f'[{_id}] Starting {" ".join(argv)} at t={t0}')
     proc = subprocess.Popen(argv, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, shell=False)
+
+    t0 = time.time()
 
     while True:
         exit_code = proc.poll()
         if exit_code is None:
             # Process is still running
             if timeout > 0 and time.time() - t0 >= timeout:
-                print(f'[{_id}] Time is up!')
                 proc.kill()
-                return None
+                raise Exception('Timeout exceeded')
         else:
-            print(f'[{_id}] {" ".join(argv)} exited; exit code {exit_code}')
-            return proc
+            return proc.returncode
         # time.sleep(1)     <-- BAD idea
         yield 20 * TICK
 
 
-def defcallback(result):
-    print(f'[callback] result={result}')
+def defcallback(coro_id, result):
+    print(f'[{coro_id}] result={result}')
 
 
-def deferrback(error):
-    print(f'[errback] exception={error}')
+def deferrback(coro_id, error):
+    print(f'[{coro_id}] exception={error}')
+
+
+class Loop:
+    def __init__(self):
+        self.now = 0
+        self.ready_at = defaultdict(list)
+        self.coro_ids = {}
+        self.number_of_coroutines = 0
+
+    def schedule(self, coro, clbk=defcallback, erbk=deferrback, t=0):
+        t = round(t, RESOLUTION)
+        if t <= self.now:
+            t = self.now
+
+        self.ready_at[t].append((coro, clbk, erbk))
+        if coro not in self.coro_ids:
+            self.coro_ids[coro] = str(uuid.uuid4())
+            self.number_of_coroutines += 1
+        if t == 0:
+            print(f'[loop] coro {self.coro_ids[coro]} scheduled at t={t}')
+
+    def run(self):
+        while self.number_of_coroutines:
+            self.now = round(self.now, RESOLUTION)
+
+            while self.ready_at[self.now]:
+                coro, clbk, erbk = self.ready_at[self.now].pop(0)
+                try:
+                    run_next = round(self.now + coro.send(None), RESOLUTION)
+                except StopIteration as e:
+                    clbk(self.coro_ids[coro], e.value)
+                    self.number_of_coroutines -= 1
+                except Exception as e:
+                    erbk(self.coro_ids[coro], e)
+                    self.number_of_coroutines -= 1
+                else:
+                    self.schedule(coro, clbk, erbk, run_next)
+            self.now += TICK
+            time.sleep(TICK)
 
 
 if __name__ == '__main__':
-    now = 0
-    ready_at = defaultdict(list)
-    coro_ids = {}
+    loop = Loop()
 
-    ready_at[now] = [(runner('sleep 10'.split()), defcallback, deferrback),
-                     (runner('sleep 10'.split()), defcallback, deferrback),
-                     (runner('sleep 10'.split()), defcallback, deferrback),
-                     (runner('sleep 10'.split()), defcallback, deferrback),
-                     (runner('sleep 10'.split()), defcallback, deferrback),
-                     (runner('sleep 10'.split()), defcallback, deferrback)]
-    number_of_coroutines = len(ready_at[now])
+    loop.schedule(runner('sleep 10'.split(), timeout=5))
+    loop.schedule(runner('sleep 10'.split()))
+    loop.schedule(runner('sleep 10'.split()))
+    loop.schedule(runner('sleep 10'.split()))
+    loop.schedule(runner('sleep 10'.split()))
 
-    while number_of_coroutines:
-        now = round(now, RESOLUTION)
-
-        while ready_at[now]:
-            coro, clbk, erbk = ready_at[now].pop(0)
-            try:
-                run_next = round(now + coro.send(None), RESOLUTION)
-            except StopIteration as e:
-                clbk(e.value.returncode)
-                number_of_coroutines -= 1
-            except Exception as e:
-                erbk(e)
-                number_of_coroutines -= 1
-            else:
-                # print(f'[loop] waking up coroutine at t={now + run_next}')
-                ready_at[run_next].append((coro, clbk, erbk))
-        now += TICK
-        time.sleep(TICK)
+    loop.run()
