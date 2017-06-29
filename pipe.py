8f9ecaee17f0bcf12d740040eca97e5a3e08070d
Francesco Pierfederici <fpierfed@gmail.com>
2017-06-29 15:23:22 +0200

    somewhat better

--- pipe.py
+++ pipe.py
@@ -1,72 +1,72 @@
 from collections import defaultdict
-from functools import wraps
 import subprocess
 import time
 import uuid
 
 
-TICK = 0.01    # one simulation tick in seconds
-PRCS = 2
-
-
-def coroutine(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwds):
-        coro = fn(*args, **kwds)
-        coro.send(None)
-        return coro
-    return wrapper
+TICK = 0.001
+RESOLUTION = 3
 
 
 def runner(argv, timeout=0):
     _id = str(uuid.uuid4())
 
     t0 = time.time()
-
-    print(f'[{_id}] Running {" ".join(argv)} at {t0}')
-    proc = subprocess.Popen(argv,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            shell=False)
+    print(f'[{_id}] Starting {" ".join(argv)} at t={t0}')
+    proc = subprocess.Popen(argv, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=False)
 
     while True:
         exit_code = proc.poll()
         if exit_code is None:
+            # Process is still running
             if timeout > 0 and time.time() - t0 >= timeout:
                 print(f'[{_id}] Time is up!')
+                proc.kill()
                 return None
         else:
-            print(f'[{_id}] {" ".join(argv)} done.')
+            print(f'[{_id}] {" ".join(argv)} exited; exit code {exit_code}')
             return proc
-        # time.sleep(10)    # <-- BAD idea
-        yield TICK * 20
+        # time.sleep(1)     <-- BAD idea
+        yield 20 * TICK
 
 
-def generic_callback(result):
-    print(result)
+def defcallback(result):
+    print(f'[callback] result={result}')
+
+
+def deferrback(error):
+    print(f'[errback] exception={error}')
 
 
 if __name__ == '__main__':
-    # Scheduler/event loop
     now = 0
     ready_at = defaultdict(list)
-    ready_at[now] = [(runner(argv), generic_callback)
-                     for argv in ['sleep 5'.split(),
-                                  'sleep 10'.split()]]
+    coro_ids = {}
+
+    ready_at[now] = [(runner('sleep 10'.split()), defcallback, deferrback),
+                     (runner('sleep 10'.split()), defcallback, deferrback),
+                     (runner('sleep 10'.split()), defcallback, deferrback),
+                     (runner('sleep 10'.split()), defcallback, deferrback),
+                     (runner('sleep 10'.split()), defcallback, deferrback),
+                     (runner('sleep 10'.split()), defcallback, deferrback)]
     number_of_coroutines = len(ready_at[now])
 
     while number_of_coroutines:
-        now = round(now, PRCS)
+        now = round(now, RESOLUTION)
 
         while ready_at[now]:
-            coro, clbk = ready_at[now].pop(0)
+            coro, clbk, erbk = ready_at[now].pop(0)
             try:
-                run_next = round(now + coro.send(None), PRCS)
+                run_next = round(now + coro.send(None), RESOLUTION)
             except StopIteration as e:
+                clbk(e.value.returncode)
                 number_of_coroutines -= 1
-                clbk(e.value)
+            except Exception as e:
+                erbk(e)
+                number_of_coroutines -= 1
             else:
-                print(f'[loop] waking up coroutine at t={run_next}')
-                ready_at[run_next].append((coro, clbk))
+                # print(f'[loop] waking up coroutine at t={now + run_next}')
+                ready_at[run_next].append((coro, clbk, erbk))
         now += TICK
         time.sleep(TICK)
