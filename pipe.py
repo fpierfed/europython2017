c52ef9e16b430184d016f28aa0da13fec80e128e
Francesco Pierfederici <fpierfed@gmail.com>
2017-07-04 14:42:56 +0200

    refactored heavily to introduce Tasks and simpler API

--- pipe.py
+++ pipe.py
@@ -32,37 +32,66 @@ def runner(argv, timeout=0):
         yield from sleep(20)
 
 
-def defcallback(coro_id, result):
-    print(f'[{coro_id}] result={result}')
+def defcallback(task):
+    print(f'[{task.id}] result={task.result}')
 
 
-def deferrback(coro_id, error):
-    print(f'[{coro_id}] exception={error}')
+def deferrback(task):
+    print(f'[{task.id}] exception={task.exception}')
+
+
+class Task:
+    def __init__(self, coroutine, callback=defcallback, errback=deferrback):
+        self.coroutine = coroutine
+        self.id = str(uuid.uuid4())
+        self.callback = callback
+        self.errback = errback
+
+    @property
+    def result(self):
+        return self._result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
+
+    @property
+    def exception(self):
+        return self._exception
+
+    @exception.setter
+    def exception(self, value):
+        self._exception = value
 
 
 class Loop:
     def __init__(self):
         self.now = 0
         self.ready_at = defaultdict(list)
-        self.coro_ids = {}
-        self.number_of_coroutines = 0
+        self.all_tasks = []
 
-    def schedule(self, coro, clbk=defcallback, erbk=deferrback, t=0):
+    def schedule(self, task, t=0):
         t = round(t, RESOLUTION)
         if t <= self.now:
             t = self.now
 
-        self.ready_at[t].append((coro, clbk, erbk))
-        if coro not in self.coro_ids:
-            self.coro_ids[coro] = str(uuid.uuid4())
-            self.number_of_coroutines += 1
+        if not isinstance(task, Task):
+            task = Task(task)
+
+        self.ready_at[t].append(task)
+        if task not in self.all_tasks:
+            self.all_tasks.append(task)
         if t == 0:
-            print(f'[loop] coro {self.coro_ids[coro]} scheduled at t={t}')
+            print(f'[loop] task {task.id} scheduled at t={t}')
+
+    def remove(self, task):
+        if task in self.all_tasks:
+            self.all_tasks.remove(task)
 
     def run(self):
         sel = selectors.DefaultSelector()
 
-        while self.number_of_coroutines:
+        while self.all_tasks:
             self.now = round(self.now, RESOLUTION)
 
             events = sel.select()
@@ -71,17 +100,20 @@ class Loop:
                 callback(key.fileobj, mask)
 
             while self.ready_at[self.now]:
-                coro, clbk, erbk = self.ready_at[self.now].pop(0)
+                task = self.ready_at[self.now].pop(0)
                 try:
-                    run_next = round(self.now + coro.send(None), RESOLUTION)
+                    run_next = round(self.now +
+                                     next(task.coroutine), RESOLUTION)
                 except StopIteration as e:
-                    clbk(self.coro_ids[coro], e.value)
-                    self.number_of_coroutines -= 1
+                    task.result = e.value
+                    task.callback(task)
+                    self.remove(task)
                 except Exception as e:
-                    erbk(self.coro_ids[coro], e)
-                    self.number_of_coroutines -= 1
+                    task.exception = e
+                    task.errback(task)
+                    self.remove(task)
                 else:
-                    self.schedule(coro, clbk, erbk, run_next)
+                    self.schedule(task, t=run_next)
             self.now += TICK
             time.sleep(TICK)
 
