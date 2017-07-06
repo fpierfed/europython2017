f4c68a4ad1b3a5a5baeb75a310ef23b32bfed63b
Francesco Pierfederici <fpierfed@gmail.com>
2017-07-06 11:08:37 +0200

    initial

---
+++ executor_example.py
@@ -0,0 +1,48 @@
+import asyncio
+import concurrent.futures as cf
+import time
+
+
+def long_running_function(n):
+    """Sleep for n seconds."""
+    time.sleep(n)
+
+
+def high_cpu_function(n):
+    """Burn CPU cycles for n seconds."""
+    t0 = time.time()
+
+    i = 0
+    while time.time() - t0 < n:
+        i += 1
+    return i
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    # BAD!
+    # loop.run_until_complete(asyncio.wait([
+    #     long_running_function(10),
+    #     long_running_function(10),
+    #     high_cpu_function(10),
+    #     high_cpu_function(10),
+    # ]))
+
+    # Better
+    # executor = cf.ThreadPoolExecutor()
+    # loop.run_until_complete(asyncio.wait([
+    #     loop.run_in_executor(executor, long_running_function, 10),
+    #     loop.run_in_executor(executor, long_running_function, 10),
+    #     loop.run_in_executor(executor, high_cpu_function, 10),
+    #     loop.run_in_executor(executor, high_cpu_function, 10),
+    # ]))
+
+    # Best?
+    executor = cf.ProcessPoolExecutor()
+    loop.run_until_complete(asyncio.wait([
+        loop.run_in_executor(executor, long_running_function, 10),
+        loop.run_in_executor(executor, long_running_function, 10),
+        loop.run_in_executor(executor, high_cpu_function, 10),
+        loop.run_in_executor(executor, high_cpu_function, 10),
+    ]))
